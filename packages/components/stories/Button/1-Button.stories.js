6347095d96aa855944d0be7168a0571387672d5f
joao tavares <joao.arthur.tavares@gmail.com>
2020-07-14 21:11:46 -0300

    feat(Megazord/Styleguide): add @megazord/styleguide lib

--- packages/components/stories/Button/1-Button.stories.js
+++ packages/components/stories/Button/1-Button.stories.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
+import { theme } from '@megazord/styleguide'
 
 export default {
   title: 'Button',
   component: Button,
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const Text = () => <Button onClick={action('clicked')}>
+  <span style={{backgroundColor: theme.palette.primary.main, color: theme.palette.primary.contrastText}}>Hello World!</span>
+</Button>;
 
 export const Emoji = () => (
   <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
+    <span role="img" aria-label="so cool" >
       😀 😎 👍 💯
     </span>
   </Button>
